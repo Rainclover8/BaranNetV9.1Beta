0cac52d39cf6b79ffd41c9a902404cdcfd4fefe8
merahab <ts.baran6161@gmail.com>
2025-01-08 18:19:09 +0300

    ah ah

--- backend/src/api/proje/controllers/proje.ts
+++
@@ -1,7 +0,0 @@
-/**
- * proje controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::proje.proje');
--- backend/utils/envHelper.ts
+++
@@ -1,5 +0,0 @@
-export const envHelper = (env: (key: string, defaultValue?: any) => any) => ({
-    int: (key: string, defaultValue?: number) => parseInt(env(key, defaultValue), 10),
-    bool: (key: string, defaultValue?: boolean) => env(key, defaultValue) === 'true',
-  });
-  
